7d39326e13f32e2b7b189669f34d95c667684f28
Leo McArdle <leomcardle@gmail.com>
2018-08-02 14:09:16 +0100

    bump version

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: sidekiq-monitor
 # about: Job that sends a ping from sidekiq to let us know it's still running
-# version: 0.0.1
+# version: 0.0.2
 # authors: Leo McArdle
 # url: https://github.com/mozilla/discourse-sidekiq-monitor
 
