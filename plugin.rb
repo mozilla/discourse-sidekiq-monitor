fb0c51fc0c37ca53ddd6f422f897c87d29e33123
Leo McArdle <leomcardle@gmail.com>
2018-08-02 13:34:22 +0100

    initial code

---
+++ plugin.rb
@@ -0,0 +1,38 @@
+# name: sidekiq-monitor
+# about: Job that sends a ping from sidekiq to let us know it's still running
+# version: 0.0.1
+# authors: Leo McArdle
+# url: https://github.com/mozilla/discourse-sidekiq-monitor
+
+after_initialize do
+  module ::Jobs
+    class MozillaSidekiqMonitor < ::Jobs::Scheduled
+      every 5.minutes
+      sidekiq_options queue: 'critical'
+
+      def execute(args)
+        account_id = SiteSetting.newrelic_account_id
+        key = SiteSetting.newrelic_insights_key
+
+        return if account_id.blank? || key.blank?
+
+        payload = [{
+          eventType: :sidekiqRunning,
+          value: 1
+        }]
+
+        uri = URI("https://insights-collector.newrelic.com/v1/accounts/#{account_id}/events")
+        req = Net::HTTP::Post.new(uri)
+        req['X-Insert-Key'] = "#{key}"
+
+        res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
+          http.request(req)
+        end
+
+        unless res.code == "200"
+          raise "Sidekiq monitor job failed with HTTP #{res.code}: #{res.body}"
+        end
+      end
+    end
+  end
+end
---
+++ spec/plugin_spec.rb
@@ -0,0 +1,49 @@
+require "rails_helper"
+
+describe Jobs::MozillaSidekiqMonitor do
+
+  before do
+    SiteSetting.newrelic_account_id = 1234
+    SiteSetting.newrelic_insights_key = 5678
+  end
+
+  context "with newrelic account id and key set" do
+    it "uses them" do
+      stub_request(:post, "https://insights-collector.newrelic.com/v1/accounts/1234/events").with({
+        headers: {
+          "X-Insert-Key": "5678"
+        }
+      }).to_return(status: 200)
+
+      described_class.new.execute({})
+    end
+  end
+
+  context "without newrelic account id set" do
+    before { SiteSetting.newrelic_account_id = " " }
+
+    it "does nothing" do
+      described_class.new.execute({})
+    end
+  end
+
+  context "without newrelic key set" do
+    before { SiteSetting.newrelic_insights_key = " " }
+
+    it "does nothing" do
+      described_class.new.execute({})
+    end
+  end
+
+  context "when querying the endpoint fails" do
+    before do
+      stub_request(:post, "https://insights-collector.newrelic.com/v1/accounts/1234/events").to_return(status: 500)
+    end
+
+    it "raises an error" do
+      expect do
+        described_class.new.execute({})
+      end.to raise_error(RuntimeError)
+    end
+  end
+end
